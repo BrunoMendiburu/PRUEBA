58683ffd4515958c5294262321971f0e3840b62b
BrunoMendiburu <101568817+BrunoMendiburu@users.noreply.github.com>
2022-04-13 15:28:46 -0300

    PRUEBA1DINISHED

--- PRUEBA1.cs
+++ PRUEBA1.cs
@@ -4,13 +4,39 @@ using UnityEngine;
 
 public class PRUEBA1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int dias;
+    public int flotataxis;
+    public int diaslluvia; 
+
     void Start()
     {
-        
+        int combustible;
+        combustible = 130
+        int lluvia;
+        int diasgral;
+        diasgral = dias - diaslluvia;
+        lluvia= 120/15
+        total = taxis * ((lluvia * (combustible * lluvia))*(diasgral * (combustible * 6))));
+
+        if (dias < 5)
+        {
+            Debug.Log("Error, la cantidad de dias debe ser mayor a 6");
+        }
+        else if (lluvia < 0)
+        {
+            Debug.Log("Error.");
+        }
+        else if (lluvia > dias)
+        {
+            Debug.Log("Error.");
+        }
+        else
+        {
+            Debug.Log("Una flota de " + taxis + "unidades trabajando durante" + dias + "implicara un gasto de" + total + "pesos en combustible");
+        }
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
